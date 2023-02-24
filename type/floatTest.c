6fdac44023ea5fa3fa0e5758f75824f33710c6b9
zhaopeng <2212560792@qq.com>
2023-02-24 10:50:34 +0000

    update

--- type/floatTest.c
+++ type/floatTest.c
@@ -5,6 +5,7 @@
 
 int main()
 {
+#if 0
     float a = 0.3;
     float b = 0.6;
     float c = a * b;
@@ -17,5 +18,32 @@ int main()
         printf("d == 0.9\n");
 
     printf("c = %.8f\n", c);
+#endif
+
+#if 1
+    double a = 0.3;
+    double b = 6;
+    double a1 = 0.6;
+    
+    double c = a * b;
+    double d = a + b;
+    double e = a + a1;
+
+    const double EPS = 1e-6; 
+
+    if(c == 1.8)
+        printf("c == 1.8\n");
+
+    if(d == 6.3)
+        printf("d == 6.3\n");
+
+    if(e == 0.9)
+        printf("e == 0.9\n");
+
+    printf("c = %.10f\n", c);
+    printf("EPS = %.10f\n", EPS);
+
+#endif
+
     return 0;
 }
